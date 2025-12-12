b63a1449d2b24b65694899bcd9be173dbd773a5d
john@2simple.com <john@2simple.com>
2025-12-12 11:24:27 +0000

    new files

--- public/process/src/convert.ts
+++ public/process/src/convert.ts
@@ -2,6 +2,8 @@ import { optimizeGLTF } from "./optimise";
 
 const main = async () => {
     optimizeGLTF("public/mushnub.glb", "public/mushnub2.glb");
+    optimizeGLTF("public/pinguin_001.glb", "public/pinguin_002.glb");
+    optimizeGLTF("public/Wolf2.gltf", "public/Wolf3.glb");
 }
 
 main();
